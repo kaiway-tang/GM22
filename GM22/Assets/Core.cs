a04ebf5463edc8de38a25bf9654f8ffdd23a34c9
henrywangzh <henrywang3@g.ucla.edu>
2022-11-06 02:12:02 -0800

    adding cores

---
+++ GM22/Assets/Core.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Core : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Random.Range(-0.1f, 0.1f);
+        float y = Random.Range(-0.1f, 0.1f);
+        float z = Random.Range(-0.1f, 0.1f);
+        rigidbody.AddForce(new Vector3(x, y, z));
+    }
+}
--- GM22/Assets/EnemyShatter.cs
+++ GM22/Assets/EnemyShatter.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class EnemyShatter : MonoBehaviour
 {
     [SerializeField] GameObject shatteredEnemy;
+    [SerializeField] GameObject core;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == 6)
         { // collided with player
             Instantiate(shatteredEnemy, transform.position, Quaternion.identity);
+            Instantiate(core, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
             Destroy(gameObject);
         }
     }
