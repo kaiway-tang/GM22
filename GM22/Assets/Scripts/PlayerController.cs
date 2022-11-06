6d17f14421e53aa116d6b154767ae5d607356c72
Daniel Tsai <32005886+danieltsai-RSheepDV@users.noreply.github.com>
2022-11-06 04:09:11 -0800

    input changes

--- GM22/Assets/Scripts/PlayerController.cs
+++ GM22/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+    private GM22 inputs;
+    private InputAction attackInputAction;
+    private InputAction lookInputAction;
+    private InputAction moveInputAction;
+    
     Rigidbody rb;
     Animator anim;
     [SerializeField] float speed = 5f;
@@ -20,7 +27,20 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
+    }
 
+    private void OnEnable()
+    {
+        inputs = new GM22();
+        attackInputAction = inputs.Player.Attack;
+        lookInputAction = inputs.Player.Look;
+        moveInputAction = inputs.Player.Move;
+        inputs.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputs.Disable();
     }
 
     // Update is called once per frame
@@ -31,7 +51,7 @@ public class PlayerController : MonoBehaviour
             // Using isometric to test, will change later
             
             Vector3 camDir = new Vector3(cam.transform.forward.x, 0, cam.transform.forward.z);
-            Vector3 direction = (Input.GetAxis("Horizontal") * new Vector3(camDir.z, 0, -1 * camDir.x) + Input.GetAxis("Vertical") * camDir).normalized;
+            Vector3 direction = (moveInputAction.ReadValue<Vector2>().x * new Vector3(camDir.z, 0, -1 * camDir.x) + moveInputAction.ReadValue<Vector2>().y * camDir).normalized;
             
             bool notMoving = direction.magnitude == 0;
 
@@ -51,7 +71,7 @@ public class PlayerController : MonoBehaviour
         // Control movement animation
         anim.SetBool("Moving", rb.velocity.magnitude > 0.05f);
 
-        if (Input.GetMouseButton(0))
+        if (attackInputAction.triggered)
         {
             anim.SetBool("Combo", true);
             rb.velocity = new Vector3(0, rb.velocity.y, 0);
