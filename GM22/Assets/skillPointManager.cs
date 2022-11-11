79915d4ee09f425df22647cba01df5e316ac0d53
Kaiway Tang <86494513+kaiway-tang@users.noreply.github.com>
2022-11-11 13:54:05 -0800

    skill graph UI

--- GM22/Assets/skillPointManager.cs
+++ GM22/Assets/skillPointManager.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class skillPointManager : MonoBehaviour
 {
-    [SerializeField] Transform[] skillPoints; //
-    // Start is called before the first frame update
+    [SerializeField] Transform[] skillPoints; //0: attack; 1: hp; 2: speed
+    [SerializeField] Vector3[] move;
+
     void Start()
     {
-        
+        move[0] = new Vector3(-.866f,0.5f,0);
+        move[1] = new Vector3(.866f,0.5f,0);
+        move[2] = new Vector3(0,-1,0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void IncrementAttack()
     {
-        
+        ProcessIncrement(0);
+    }
+    public void IncrementHP()
+    {
+        ProcessIncrement(1);
+    }
+    public void IncrementSpeed()
+    {
+        ProcessIncrement(2);
+    }
+
+    void ProcessIncrement(int ID)
+    {
+        skillPoints[ID].localPosition += move[ID] * .2f;
+        //pointLines[0].position = skillPoints[1].position - skillPoints[0].position;
+        //pointLines[1].position = skillPoints[2].position - skillPoints[1].position;
+        //pointLines[2].position = skillPoints[0].position - skillPoints[2].position;
     }
 }
